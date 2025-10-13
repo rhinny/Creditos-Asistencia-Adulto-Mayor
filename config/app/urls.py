7f72512b3880c1067020f12298e69a22656a35e2
ayleenenen <ayleenhuang@gmail.com>
2025-10-12 22:30:07 -0300

    sistema de registro básico, falta personalizar

--- config/app/urls.py
+++ config/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
-urlpatterns = [path('login/estudiante', views.login_estudiante, name='login_estudiante'),
-               path('login/adultoM', views.login_adultoM, name='login_adultoM'),
-               path('', views.home, name='home')]
+urlpatterns = [#path('registro/estudiante', views.registro_estudiante, name='registro_estudiante'),
+               #path('registro/adultoM', views.registro_adultoM, name='registro_adultoM'),
+               path('', views.home, name='home'),
+               path('usuarios/registro/', views.registro, name="registro")]
--- config/app/views.py
+++ config/app/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     contexto = {'texto':"Home"}
     return render(request, "home.html",contexto)
 
-def login_estudiante(request):
+def registro(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "usuarios/registro.html", {'form':form})
+
+'''
+def registro_estudiante(request):
     if request.method == "POST":
         form = EstudianteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login_estudiante') #hasta ahora, redirige al index ,deberia dirigir a 'seleccion de apoyo'
+            return redirect('registro_estudiante') #hasta ahora, redirige al index ,deberia dirigir a 'seleccion de apoyo'
     else:
         form = EstudianteForm()
-    return render(request, 'login_estudiante.html',{'form':form})
+    return render(request, 'registro_estudiante.html',{'form':form})
 
-def login_adultoM(request):
+def registro_adultoM(request):
     if request.method == "POST":
         form = AdultoMForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login_adultoM') #hasta ahora, redirige al index ,deberia dirigir a 'seleccion de apoyo'
+            return redirect('registro_adultoM') #hasta ahora, redirige al index ,deberia dirigir a 'seleccion de apoyo'
     else:
         form = AdultoMForm()
-    return render(request, 'login_adultoM.html',{'form':form})
+    return render(request, 'registro_adultoM.html',{'form':form})
+'''
